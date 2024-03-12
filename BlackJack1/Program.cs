a06f5d5fa665dc0823e1ce0b7e1c210283b16a5d
NicolaiTue <nicolai.tue@gmail.com>
2024-03-12 08:24:53 +0100

    d

--- BlackJack1/Program.cs
+++ BlackJack1/Program.cs
@@ -7,11 +7,13 @@ namespace BlackJack1
     {
         public string Kulør { get; set; }
         public string Rang { get; set; }
+        public int Points { get; set; }
 
-        public Kort(string kulør, string rang)
+        public Kort(string kulør, string rang, int points)
         {
             Kulør = kulør;
             Rang = rang;
+            Points = Points;
         }
 
         public virtual void VisKortSæt()
@@ -20,29 +22,17 @@ namespace BlackJack1
         }
     }
 
-    // Klasse for et nummereret kort (1-10)
-    class Kortnummer : Kort
+    class BlackJackbord
     {
-        public Kortnummer(string kulør, int number) : base(kulør, number.ToString())
-        {
-        }
-    }
+        public List<Kort> dæk = new List<Kort>();
 
-    // Klasse for billedkort (knægt, dame, konge og Es)
-    class Billedkort : Kort
-    {
-        public Billedkort(string kulør, string rang) : base(kulør, rang)
-        {
-        }
-    }
+        List<Kort> Dealer = new List<Kort>();
 
-    class Program
-    {
-        static void Main()
-        {
-            // Opret et sæt spillekort alså en list
-            List<Kort> dæk = new List<Kort>();
+        public List<Kort> hånd = new List<Kort>();
 
+        public BlackJackbord(int antalDæk)
+        {
+            
             // Tilføj nummererede kort (1-10) for hver kulør
             foreach (var kulør in new[] { "Hjerter", "Spar", "Ruder", "Klør" })
             {
@@ -55,11 +45,80 @@ namespace BlackJack1
                 dæk.Add(new Billedkort(kulør, "Knægt"));
                 dæk.Add(new Billedkort(kulør, "Dame"));
                 dæk.Add(new Billedkort(kulør, "Konge"));
-                dæk.Add(new Billedkort(kulør, "Es"));
+                dæk.Add(new Es(kulør));
             }
+            ShuffleDæk();
+        }
+
+        public int BergenVærdi(List<Kort> spiller)
+        {
+
+            // her skal kortene på hånden udregnes husk Es værdi
+
+            return 0;
+        }
+
+        public void ShuffleDæk()
+        {
+            // Her skal dækket blandes
+        }
+
+        public void UddelStartHånd()
+        {
+            // den skal  give kort til dealer, spiller og vise dealerens første kort.
+            for (int i = 0; i < 2; i++)
+            {
+                Dealer.Add(dæk[0]);
+                dæk.RemoveAt(0);
+            }
+            for (int i = 0; i < 2; i++)//skal ændres i tilfælde af flere hænder spillere
+            {
+                hånd.Add(dæk[0]);
+                dæk.RemoveAt(0);
+            }
+            //print info
+        }
+
+
+
+    }
+
+
+    // Klasse for et nummereret kort (1-10)
+    class Kortnummer : Kort
+    {
+        public Kortnummer(string kulør, int number) : base(kulør, number.ToString(), number)
+        {
+        }
+    }
+
+    // Klasse for billedkort (knægt, dame, konge og Es)
+    class Billedkort : Kort
+    {
+        public Billedkort(string kulør, string rang) : base(kulør, rang,10)
+        {
+        }
+    }
+
+    class Es : Kort
+    {
+        // es points er sat til 1 for man skal tjekke den hver gang den regner pointsne sammen og hvis det er under 21 plusser den med 10
+        public Es (string kulør) : base(kulør, "Es", 1)
+        {
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+
+            BlackJackbord bord = new BlackJackbord(2);
+
+           
 
             Console.WriteLine("Sæt af spillekort:");
-            foreach (var Kort in dæk)
+            foreach (var Kort in bord.dæk)
             {
                 Kort.VisKortSæt();
             }
